babe22d48072f755b58943822ab87d3517d7223c
DavidFrontendDev <daviddevbackend@gmail.com>
2025-03-09 13:33:13 +0100

    Validando form

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,10 @@
 import {
   CryptoCurrencyResponseSchema,
   CurrencySchema,
+  PairSchema,
 } from "../schema/crypto-schema";
 import { z } from "zod";
 
 export type Currency = z.infer<typeof CurrencySchema>;
 export type CryptoCurrency = z.infer<typeof CryptoCurrencyResponseSchema>;
+export type Pair = z.infer<typeof PairSchema>;
